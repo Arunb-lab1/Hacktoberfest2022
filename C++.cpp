3512f11c9fe71a7e60bf557e3f8d840c5b7e6726
AVINAB <111741648+MrAvinab@users.noreply.github.com>
2022-10-14 18:16:02 +0530

    What is an abstraction in C++?

---
+++ C++.cpp
@@ -0,0 +1,7 @@
+//What is an abstraction in C++?
+
+
+// Abstraction means displaying the essential details to the user while hiding the irrelevant or particular details that you don’t want to show to a user. It is of two types:
+
+// Control abstraction
+// Data abstraction
