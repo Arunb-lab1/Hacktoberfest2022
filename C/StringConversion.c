c2f98189c61d0c53f92b39849aac7b98bc43833e
Arindam Das <92679140+arindamdas13@users.noreply.github.com>
2022-10-14 22:11:45 +0530

    Create StringConversion.c

---
+++ C/StringConversion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ #include <conio.h>
+int main ()
+{
+   int c = 0;
+   char ch, s[1000];
+
+   printf("Input a string: \n");
+   gets(s);
+
+   while (s[c] != '\0') {
+      ch = s[c];
+      if (ch >= 'A' && ch <= 'Z')
+         s[c] = s[c] + 32;
+      else if (ch >= 'a' && ch <= 'z')
+         s[c] = s[c] - 32;
+      c++;
+   }
+
+   printf("%s\n", s);
+
+   return 0;
+}
